f19b1eb22ed0cdfad32f3035b28e4b4c2defe40e
ByronCrowhurst <BC216857@falmouth.ac.uk>
2018-10-10 12:34:10 +0100

    Forgot to include player.py in previous push

---
+++ player.py
@@ -0,0 +1,10 @@
+class Player:
+    def loadSprite(self, direction):
+        if direction == "UP":
+            # load up sprite
+        if direction == "RIGHT":
+            # load right sprite
+        if direction == "DOWN":
+            # load down sprite
+        if direction == "LEFT":
+            # load left sprite
